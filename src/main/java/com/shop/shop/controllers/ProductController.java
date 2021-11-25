186d0464437f94307ef4a41dcaf4f5fb48a62abb
Fabala Dembele <dembele.fabala@gmail.com>
2021-11-25 15:02:25 +0100

    add categoryDao + categoryController + get all categories request

--- src/main/java/com/shop/shop/controllers/ProductController.java
+++ src/main/java/com/shop/shop/controllers/ProductController.java
@@ -6,15 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.data.web.SortDefault;
 
 import net.minidev.json.JSONObject;
 
-import javax.servlet.http.HttpServletRequest;
-
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/products")
---
+++ src/main/java/com/shop/shop/services/CategoryDao.java
@@ -0,0 +1,29 @@
+package com.shop.shop.services;
+
+import com.shop.shop.models.Category;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import net.minidev.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class CategoryDao {
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    public List<Category> listAll() {
+        /**
+         * Search and return the all list of categories
+         *
+         * @return the list of all categories
+         */
+        String sql = "SELECT * FROM categories;";
+        List<Category> list = jdbcTemplate.query(sql, BeanPropertyRowMapper.newInstance(Category.class));
+        return list;
+    }
+}
